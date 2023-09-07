200dcec2b0934b8e07fa81f19fe80610d4a7bf48
alexandr_grigorev_kt_41_20 <sgrigorev123@gmail.com>
2023-09-07 20:13:41 +0300

    add initial solution

---
+++ Lab1_alexandr_grigorev_kt_41_20/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Lab1_alexandr_grigorev_kt_41_20
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
